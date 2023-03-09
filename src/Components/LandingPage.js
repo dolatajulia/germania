1d137295d8b37080c2f2839fb46f1cba3096cdf9
jdolata <juliadolata007@gmail.com>
2023-03-09 18:59:45 +0100

    fix landing page formating

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -5,39 +5,38 @@ const LandingPage = () => {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        {/* <h1>
-        <span>Jestem tlumaczem przysieglym jezyka niemieckiego.</span> <br /> Serdecznie
-        zapraszam do skorzystania z uslug mojego biura.
-      </h1>
-      <p>
-        Przedmiotem mojej działalności są tłumaczenia z języka niemieckiego na
-        język polski, oraz z języka polskiego na język niemiecki. Moja oferta
-        skierowana jest zarówno do klientów indywidualnych, jak i firm.
-      </p>
-      <h2>Teresa Dolata</h2> */}
-      <div className={styles.rightbox}>
-      <LandingPageCard
-          name={"oferta"}
-          text={"Zapoznaj się z moją ofertą i zasadami wyceny"}
-        />
-      <LandingPageCard
-          name={"o mnie"}
-          text={"Poznaj moje kompetencje i uprawnienia"}
-        />
-        <LandingPageCard
-          name={"referencje"}
-          text={"Przeczytaj referencje klientów"}
-        />
-      <LandingPageCard
-          name={"lokalizajca"}
-          text={"Sprawdź dokładną lokalizację mojego biura"}
-        />
-        <LandingPageCard
-          name={"kontakt"}
-          text={"Skontaktuj się ze mną"}
-        />
-      </div>
-        
+        <div className={styles.leftbox}>
+          <h1>
+            <span>Jestem tlumaczem przysieglym jezyka niemieckiego.</span>
+            <br /> Serdecznie zapraszam do skorzystania z uslug mojego biura.
+          </h1>
+          <p>
+            Przedmiotem mojej działalności są tłumaczenia z języka niemieckiego
+            na język polski, oraz z języka polskiego na język niemiecki. Moja
+            oferta skierowana jest zarówno do klientów indywidualnych, jak i
+            firm.
+          </p>
+          <h2>Teresa Dolata</h2>
+        </div>
+        <div className={styles.rightbox}>
+          <LandingPageCard
+            name={"oferta"}
+            text={"Zapoznaj się z moją ofertą i zasadami wyceny"}
+          />
+          <LandingPageCard
+            name={"o mnie"}
+            text={"Poznaj moje kompetencje i uprawnienia"}
+          />
+          <LandingPageCard
+            name={"opinie"}
+            text={"Przeczytaj referencje klientów"}
+          />
+          <LandingPageCard
+            name={"biuro"}
+            text={"Sprawdź dokładną lokalizację mojego biura"}
+          />
+          <LandingPageCard name={"kontakt"} text={"Skontaktuj się ze mną"} />
+        </div>
       </div>
     </div>
   );
--- src/Components/LandingPageCard.js
+++ src/Components/LandingPageCard.js
@@ -3,11 +3,13 @@ import styles from "./LandingPageCard.module.scss";
 const LandingPageCard = (props) => {
   return (
     <div className={styles.container}>
-      <p className={styles.rotate}>{props.name}</p>
+        <div className={styles.contentbox}>
+        <p className={styles.rotate}>{props.name}</p>
       <div className={styles.textbox}>
         <p>{props.text}</p>
         <button className={styles.rightarrow}></button>
       </div>
+        </div>
     </div>
   );
 };
