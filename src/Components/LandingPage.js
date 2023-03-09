68b9ea32e8b37e9fcae0887c775b25983f95ef86
jdolata <juliadolata007@gmail.com>
2023-03-09 18:12:05 +0100

    add landing page card component

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,10 +1,11 @@
 import styles from "./LandingPage.module.scss";
+import LandingPageCard from "./LandingPageCard";
 
 const LandingPage = () => {
   return (
     <div className={styles.container}>
-        <div className={styles.content}>
-        <h1>
+      <div className={styles.content}>
+        {/* <h1>
         <span>Jestem tlumaczem przysieglym jezyka niemieckiego.</span> <br /> Serdecznie
         zapraszam do skorzystania z uslug mojego biura.
       </h1>
@@ -13,9 +14,31 @@ const LandingPage = () => {
         język polski, oraz z języka polskiego na język niemiecki. Moja oferta
         skierowana jest zarówno do klientów indywidualnych, jak i firm.
       </p>
-      <h2>Teresa Dolata</h2>
-        </div>
-      
+      <h2>Teresa Dolata</h2> */}
+      <div className={styles.rightbox}>
+      <LandingPageCard
+          name={"oferta"}
+          text={"Zapoznaj się z moją ofertą i zasadami wyceny"}
+        />
+      <LandingPageCard
+          name={"o mnie"}
+          text={"Poznaj moje kompetencje i uprawnienia"}
+        />
+        <LandingPageCard
+          name={"referencje"}
+          text={"Przeczytaj referencje klientów"}
+        />
+      <LandingPageCard
+          name={"lokalizajca"}
+          text={"Sprawdź dokładną lokalizację mojego biura"}
+        />
+        <LandingPageCard
+          name={"kontakt"}
+          text={"Skontaktuj się ze mną"}
+        />
+      </div>
+        
+      </div>
     </div>
   );
 };
---
+++ src/Components/LandingPageCard.js
@@ -0,0 +1,15 @@
+import styles from "./LandingPageCard.module.scss";
+
+const LandingPageCard = (props) => {
+  return (
+    <div className={styles.container}>
+      <p className={styles.rotate}>{props.name}</p>
+      <div className={styles.textbox}>
+        <p>{props.text}</p>
+        <button className={styles.rightarrow}></button>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPageCard;
